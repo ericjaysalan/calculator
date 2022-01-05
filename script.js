aba816246ca84bdc3eeb4978bdd181ffef93489e
Eric Jay Salan <ericjaysalan@yahoo.com>
2022-01-05 18:22:15 +0800

    Refactored arithmetic code. Now can take multiple numbers instead of just 2.

--- script.js
+++ script.js
@@ -4,32 +4,7 @@ const buttons = Array.from(document.getElementsByTagName('button'));
 const numberButtons = Array.from(document.getElementsByClassName('numbers'));
 const operatorButtons = Array.from(document.getElementsByClassName('operator'));
 
-// equation.innerText = '1';
-let input = {};
-let operatorClicked = false;
-
-function operate(input) {
-  let result;
-
-  switch (input.operator) {
-    case '+':
-      result = add(input);
-      break;
-    case '-':
-      result = subtract(input);
-      break;
-    case 'x':
-      result = multiply(input);
-      break;
-    case '÷':
-      result = divide(input);
-      break;
-    default:
-      console.warn('Operator not found.');
-  }
-
-  return result;
-}
+let input = [];
 
 const add = (input) => input.operand1 + input.operand2;
 
@@ -39,79 +14,92 @@ const multiply = (input) => input.operand1 * input.operand2;
 
 const divide = (input) => input.operand1 / input.operand2;
 
+function operate(arr) {
+  let result;
+  let input = {
+    operand1: -1,
+    operator: '',
+    operand2: -1,
+  };
+
+  for (let i = 0; i < arr.length; ) {
+    if (arr.length === 1) break;
+
+    input.operand1 = arr[i];
+    input.operator = arr[i + 1];
+    input.operand2 = arr[i + 2];
+
+    switch (input.operator) {
+      case '+':
+        result = add(input);
+        break;
+      case '-':
+        result = subtract(input);
+        break;
+      case 'x':
+        result = multiply(input);
+        break;
+      case '÷':
+        result = divide(input);
+        break;
+      default:
+        console.warn('Operator not found.');
+    }
+
+    arr.splice(0, 3, result);
+  }
+
+  return arr[0];
+}
+
 const appendInput = (inputString) => (display.innerText += inputString);
 
 const isEqualsButton = (innerText) => innerText.includes('=');
 
 const clearDisplay = () => (display.innerText = '');
 
-const checkInput = (input) => {
-  return 'operand1' in input && 'operator' in input && 'operand2' in input;
-};
+const checkInput = (input) => !isNaN(Number(input));
 
 const displayIsNotEmpty = () => display.innerText;
 
-function deleteInputProperties(input) {
-  delete input.operand1;
-  delete input.operand2;
-  delete input.operator;
-}
+const displayEquation = (input) => (equation.innerText = input.join(' '));
 
-function displayEquation(input, equals = false) {
-  if (equals) {
-    equation.innerText = `${input.operand1} ${input.operator} ${input.operand2}`;
-  } else {
-    equation.innerText = input.operand1;
-  }
+function clear() {
+  input = [];
+  display.innerText = '';
+  equation.innerText = '';
 }
 
 function displayInput(e) {
+  let displayText = display.innerText;
   const target = e.target;
   const className = target.className;
 
-  if (className.includes('clear')) {
-    display.innerText = '';
-    equation.innerText = '';
-    deleteInputProperties(input);
-  } else if (className.includes('operator')) {
-    operatorClicked = true;
+  if (className.includes('clear')) clear();
+  else if (className.includes('operator')) {
+    if (displayIsNotEmpty() && checkInput(displayText)) {
+      switch (target.innerText) {
+        case '=':
+          input.push(Number(display.innerText));
+          display.innerText = operate(input);
 
-    switch (target.innerText) {
-      case '=':
-        if ('operand1' in input && displayIsNotEmpty()) {
-          input.operand2 = Number(display.innerText);
-
-          if (checkInput(input)) {
-            display.innerText = operate(input);
-            displayEquation(input, true);
-          }
-        }
-        break;
-      case '+':
-      case '-':
-      case 'x':
-      case '÷':
-        input.operator = target.innerText;
-      default:
-        if (displayIsNotEmpty()) {
-          input.operand1 = Number(display.innerText);
-          displayEquation(input);
+          break;
+        case '+':
+        case '-':
+        case 'x':
+        case '÷':
+        default:
+          input.push(Number(displayText));
+          input.push(target.innerText);
 
           clearDisplay();
-        }
-        break;
+          break;
+      }
     }
+    displayEquation(input);
   } else if (className.includes('number')) {
     appendInput(target.innerText);
   }
 }
-
-/* let input = '3 / 2';
-input = input.split(' ');
-input = {
-  operand1: Number(input[0]),
-  operator: input[1],
-  operand2: Number(input[2]),
-}; */
 
 buttons.forEach((button) => addEventListener('click', displayInput));
