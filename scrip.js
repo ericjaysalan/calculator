c182d2b6544909b43f562758812633cd30ae09bd
Eric Jay <44886343+ericjaysalan@users.noreply.github.com>
2025-03-18 17:40:54 +0800

    Add functionality to determine which numbers to operate on and which operator to use

--- scrip.js
+++ scrip.js
@@ -1,8 +1,13 @@
 const screen = document.querySelector("#screen");
 const buttonWrapper = document.querySelector("#button-wrapper");
 
-let currentNumber = 0;
-let previousNumber = 0;
+const numbersToOperate = {
+  inputDone: false,
+  firstNumber: null,
+  secondNumber: null,
+  operator: null,
+  operatorSelected: false,
+};
 
 const clearScreen = () => {
   screen.textContent = "0";
@@ -11,20 +16,39 @@ const clearScreen = () => {
 const showOnScreen = (buttonName) => {
   if (screen.textContent === "0") {
     screen.textContent = buttonName;
+  } else if (numbersToOperate.inputDone) {
+    clearScreen();
+    screen.textContent = buttonName;
+    numbersToOperate.inputDone = false;
   } else {
     screen.textContent += buttonName;
   }
 };
 
+const process = (currentNumber, operator) => {
+  numbersToOperate.operatorSelected = true;
+  numbersToOperate.operator = operator;
+
+  if (numbersToOperate.firstNumber === null) {
+    numbersToOperate.firstNumber = currentNumber;
+    numbersToOperate.inputDone = true;
+  } else if (numbersToOperate.secondNumber === null) {
+    numbersToOperate.secondNumber = currentNumber;
+    numbersToOperate.inputDone = true;
+    alert(operate);
+  }
+
+  console.log(numbersToOperate);
+};
+
 buttonWrapper.addEventListener("click", (clickEvent) => {
   const button = clickEvent.target;
 
-  if (
-    button.classList.contains("numbers") ||
-    button.classList.contains("operators")
-  ) {
+  if (button.classList.contains("numbers")) {
     showOnScreen(button.textContent);
   } else if (button.classList.contains("clear")) {
     clearScreen();
+  } else if (button.classList.contains("operators")) {
+    process(screen.textContent, button.textContent);
   }
 });
